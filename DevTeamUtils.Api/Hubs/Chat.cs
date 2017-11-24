632933e25134427c08a2e0bea4cac32fd0f75f61
Charlestown Dias da Cunha <cdcunha@gmail.com>
2017-11-24 16:42:36 -0200

    Pasta renomeada para Hubs

---
+++ DevTeamUtils.Api/Hubs/Chat.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevTeamUtils.Api.SignalRHubs
+{
+    public class Chat : Hub
+    {
+        /*public override async Task OnConnectedAsync()
+        {
+            //await Clients.Client(Context.ConnectionId).InvokeAsync("SetUsersOnline", await GetUsersOnline());
+
+            await base.OnConnectedAsync();
+        }*/
+
+        public async Task OnUsersJoined()
+        {
+            await Clients.Client(Context.ConnectionId).InvokeAsync("UsersJoined", "");
+        }
+
+        public async Task OnUsersLeft()
+        {
+            await Clients.Client(Context.ConnectionId).InvokeAsync("UsersLeft", "");
+        }
+
+        public async Task SendAll(string message)
+        {   
+            await Clients.All.InvokeAsync("update", Context.User.Identity.Name, message);
+        }
+
+        public async Task SendTo(string toId, string message)
+        {
+            await Clients.Client(toId).InvokeAsync("message2Me", 
+                Context.User.Identity.Name + "<span style='color: red'> in PVT</span>", 
+                message);
+        }
+
+        public async Task Join()
+        {   
+            List<string> excludedIds = new List<string> { Context.ConnectionId };
+            string message = Context.User.Identity.Name + " has join the server.";
+            await Clients.Client(Context.ConnectionId).InvokeAsync("messageMe", Context.User.Identity.Name);
+            await Clients.AllExcept(excludedIds).InvokeAsync("update", message);
+            foreach (System.Security.Principal.IIdentity identity in Context.User.Identities)
+            {
+
+            }
+        }
+    }
+}
