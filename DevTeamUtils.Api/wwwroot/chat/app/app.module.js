46ebc69287878cf43f3a65c99dca262b942c1d76
Charlestown Dias da Cunha <cdcunha@gmail.com>
2017-11-30 13:33:22 -0200

    Acrescentado Identity para Login de usuário

--- DevTeamUtils.Api/wwwroot/chat/app/app.module.js
+++ DevTeamUtils.Api/wwwroot/chat/app/app.module.js
@@ -1,4 +1,18 @@
-﻿angular.module('chatDevTeam', ['ngRoute', 'ngMessages']);
+﻿angular.module('chatDevTeam', ['ngRoute', 'ngMessages']).directive('myLink', function () {
+    return {
+        restrict: 'A',
+        scope: {
+            enabled: '=myLink'
+        },
+        link: function (scope, element, attrs) {
+            element.bind('click', function (event) {
+                if (!scope.enabled) {
+                    event.preventDefault();
+                }
+            });
+        }
+    };
+});
 
 angular.element(document).ready(function () {
     angular.bootstrap(document, ['chatDevTeam']);
